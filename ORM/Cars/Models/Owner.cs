17cebeb8470d1ea6952966c701c7ebcd56b9ad64
Karol Kobiela <kobiaszu@gmail.com>
2017-11-18 15:39:16 +0100

    Added Cars task

---
+++ ORM/Cars/Models/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cars.Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public virtual ICollection<Car> CarList { get; set; }
+    }
+}
